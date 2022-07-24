bebfb9f290975ed3eeb590a6668da343d0afaf1c
MDgaddis <matthew.gaddis@wsu.edu>
2022-07-24 16:40:11 -0700

    Update midpoint.py

--- Python/midpoint.py
+++ Python/midpoint.py
@@ -15,17 +15,18 @@ def midpoint(f,a=0,b=1,n=10):
     h = (b-a)/n
     heights = []
     total = 0   # Calculating the total
-    x = np.arange(a,b,h)
+    x = np.arange(a,b+h,h)
+    
     for i in range(n):
         total += f((x[i] + (x[i+1]))/2)*(h)       # Calculating the total
-        heights.append(f((i*h + (i+1)*h)/2))    # Array of the heights given by the midpoint of the function
+        heights.append(f((x[i] + x[i+1])/2))    # Array of the heights given by the midpoint of the function
         
     # x values for the methods   
     x1 = np.arange((a+h/2),(b+h/2),h)
     
     # plots for the methods
-    plt.plot(x1,f(x1))  
-    plt.bar(x,heights, align = 'edge', color = 'orange', edgecolor = 'blue', width = h, alpha = 0.5)
+    plt.plot(x,f(x))  
+    plt.bar(x1, heights, align = 'center', color = 'orange', edgecolor = 'blue', width = h, alpha = 0.5)
     
 def func(x):
     return x*x
